c814182ac0583f73fad28382f91ec8a868665b07
Raghav Batra <rb698@cornell.edu>
2017-06-11 16:19:59 +0530

    footer + gradients

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,11 @@
 <?php
-    echo '<ol id = "footer">
-        <li><a href = "#">Footer</a>
-    </ol>';
+        echo '<ol id = "footer">
+            <div id = "float_left">
+                <li><a href = "#">&copy; 2017</a>
+            </div>
+            <div id = "float_right">
+                <li><a href = "#">Subscribe</a>
+                <li><a href = "#">Contact Us</a>
+            </div>
+        </ol>';
 ?>  

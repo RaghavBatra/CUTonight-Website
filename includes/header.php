9133cf09d98a153a4cee51826300c35ded160af6
Raghav Batra <rb698@cornell.edu>
2017-06-22 19:47:34 +0530

    Home page finished with changes

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,7 @@
     echo '<head>';
     echo '<link rel="stylesheet" type="text/css" href="css/styles.css">';
     echo '<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">';
+    echo '<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">';
     echo '<ol id = "header">
             <li id = "title"><a href = "#">CUTonight Commission</a>
             <li><a href = "#">Home</a>
--- index.php
+++ index.php
@@ -7,20 +7,38 @@
     <body>
         
         <div id = "content">
+                
+                <h1 id="welcome"> Welcome to the CUTonight Commission! </h1>
+                
+                <img src="images/logo.gif" id="logo">
+                
+                <p id="main_para">
+                    CUTonight is a byline-funded organization that provides event-oriented funding to student organizations for innovative late-night programs at Cornell University.
             
-            <div id = "left_part">
-                <?php
-//                ini_set('display_errors', 1);
-//                mail("rb698@cornell.edu", "hi", "dgfdg");
-//                echo phpinfo();
-                ?>
+                    We fund events that share our mission of enhancing social environment at Cornell.
+                </p>
+                
             </div>
             
-            <div id = "right_part">
+            <hr id="divider">
+            
+            <div id="dates">
+                
+                <p>
+                    Applications are now <b class="date_information">OPEN</b> for events being held between <b class="date_information">8/25/17</b> and <b class="date_information">10/8/17</b>. 
+                </p>
+                <p>
+                The due date for the application is <b class="date_information">4/14/17</b>.
+                </p>
+                
+                
+                <button id="apply"> 
+                    
+                    Apply now!
+                </button>
+                
             </div>
             
-        </div>
-        
         <?php
         include 'includes/footer.php';
         ?>

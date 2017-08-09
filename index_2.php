7ec3cf132a0d596a927cda91dced31b7197dee01
Raghav Batra <rb698@cornell.edu>
2017-08-09 11:54:51 +0530

    new index

---
+++ index_2.php
@@ -0,0 +1,49 @@
+<html>
+    
+    <?php
+    include 'includes/header.php';
+    ?>
+    
+    <body id = "index_body">
+        
+        <div id = "index_content">
+            <div>
+                
+                <h1> Welcome to the Blah Commission! </h1>
+                
+                <img src="images/logo.gif" id="logo">
+                
+                <p id="main_para">
+                    CUTonight is a byline-funded organization that provides event-oriented funding to student organizations for innovative late-night programs at Cornell University.
+            
+                    We fund events that share our mission of enhancing social environment at Cornell.
+                </p>
+                
+            </div>
+            
+            <hr id="divider">
+            
+            <div id="dates">
+                
+                <p>
+                    Applications are now <b class="date_information">OPEN </b> for events being held between <b class="date_information">8/25/17</b> and <b class="date_information">10/8/17</b>. 
+                </p>
+                <p>
+                The due date for the application is <b class="date_information">4/14/17</b>.
+                </p>
+                
+                
+                <button id="apply"> 
+                    
+                    Apply now!
+                </button>
+                
+            </div>
+        </div>
+            
+        <?php
+        include 'includes/footer.php';
+        ?>
+        
+    </body>
+</html>
